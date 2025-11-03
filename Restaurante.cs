e74069ded6093a7060c8e1f6fc8b23826569163b
santi290405 <200025357+santi290405@users.noreply.github.com>
2025-11-02 22:45:06 -0500

    se crea la clase restaurante
    creacion de la clase restaurante en donde se deja estipulado los gestores para proximos commits y no tener que tocar el codigo

---
+++ Restaurante.cs
@@ -0,0 +1,36 @@
+public class Restaurante
+{
+    public string Nit { get; set; }
+    public string Nombre { get; set; }
+    public string Dueno { get; set; }
+    public string Celular { get; set; }
+    public string Direccion { get; set; }
+    
+    // Gestores asociados para cuando se realicen las clases respectivas y no tener que modificar el codigo 
+    public GestorMenu Menu { get; set; }
+    public GestorClientes GestorClientes { get; set; }
+
+    public Restaurante(string nit, string nombre, string dueno, string celular, string direccion)
+    {
+        if (string.IsNullOrWhiteSpace(nit))
+            throw new ArgumentException("El NIT no puede estar vacío.");
+        if (string.IsNullOrWhiteSpace(nombre))
+            throw new ArgumentException("El nombre del restaurante no puede estar vacío.");
+        if (celular.Length != 10)
+            throw new ArgumentException("El número de celular debe tener 10 dígitos.");
+
+        Nit = nit;
+        Nombre = nombre;
+        Dueno = dueno;
+        Celular = celular;
+        Direccion = direccion;
+// Inicialización de los gestores igual para cuando se creen las clases respectivas (es para mas comodidad profe xdddd)
+        Menu = new GestorMenu();
+        GestorClientes = new GestorClientes();
+    }
+
+    public override string ToString()
+    {
+        return $"Restaurante: {Nombre} | NIT: {Nit} | Dueño: {Dueno} | Celular: {Celular} | Dirección: {Direccion}";
+    }
+}
