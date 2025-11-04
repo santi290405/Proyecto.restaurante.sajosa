70acfd93d608010395817b1895df873fab6d4820
Santia103 <santiago.loaiza.bustamante@gmail.com>
2025-11-04 16:03:41 -0500

    Cola
    En esta parte vamos a encontrar lo que es la cola en la cual vamos a poder ver si esta vacía o encolada

---
+++ Cola.cs
@@ -0,0 +1,74 @@
+public class Cola<T>
+{
+    private Nodo<T> frente;
+    private Nodo<T> final;
+
+    public Cola()
+    {
+        frente = null;
+        final = null;
+    }
+
+    
+    public bool EstaVacia()
+    {
+        return frente == null;
+    }
+
+   
+    public void Encolar(T dato)
+    {
+        Nodo<T> nuevo = new Nodo<T>(dato);
+
+        if (EstaVacia())
+        {
+            frente = nuevo;
+            final = nuevo;
+        }
+        else
+        {
+            final.Siguiente = nuevo;
+            final = nuevo;
+        }
+    }
+
+    
+    public T Desencolar()
+    {
+        if (EstaVacia())
+            throw new InvalidOperationException("No se puede desencolar: la cola está vacía.");
+
+        T dato = frente.Dato;
+        frente = frente.Siguiente;
+
+        if (frente == null)
+            final = null; 
+
+        return dato;
+    }
+
+    public T VerFrente()
+    {
+        if (EstaVacia())
+            throw new InvalidOperationException("La cola está vacía.");
+
+        return frente.Dato;
+    }
+
+    
+    public void Mostrar()
+    {
+        if (EstaVacia())
+        {
+            Console.WriteLine("La cola está vacía.");
+            return;
+        }
+
+        Nodo<T> actual = frente;
+        while (actual != null)
+        {
+            Console.WriteLine(actual.Dato);
+            actual = actual.Siguiente;
+        }
+    }
+}
